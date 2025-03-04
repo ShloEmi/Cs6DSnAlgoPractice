a540fa0d5068dd2508f02936bc9cc75d29402594
Shlomi-O <shlomi_o@hotmail.com>
2025-03-04 23:18:50 +0200

    latest

---
+++ src/Cs6DSnAlgoPractice/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Cs6DSnAlgoPractice
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
